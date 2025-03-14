556604c10e3ebae2d39a11c49cd32f545e92c273
Quaqva <sanek15056@gmail.com>
2025-03-14 22:40:31 +0400

    fix inputs

--- frontend/src/app/shared/components/input/input.component.ts
+++ frontend/src/app/shared/components/input/input.component.ts
@@ -8,7 +8,12 @@ import { InputText } from 'primeng/inputtext';
 import { InputGroupAddon } from 'primeng/inputgroupaddon';
 import { FloatLabel } from 'primeng/floatlabel';
 import { NgClass, NgTemplateOutlet } from '@angular/common';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import {
+  ControlValueAccessor,
+  FormsModule,
+  NgControl,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { faXmark } from '@fortawesome/free-solid-svg-icons';
 import type { MaskitoOptions } from '@maskito/core';
 import { maskitoCaretGuard, maskitoNumberOptionsGenerator } from '@maskito/kit';
@@ -29,21 +34,28 @@ import { FloatLabelVariants } from '../../models/float-label-variants.enum';
     ReactiveFormsModule,
     NgClass,
     MaskitoDirective,
+    FormsModule,
   ],
   templateUrl: './input.component.html',
   styleUrl: './input.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class InputComponent {
-  @Input({ required: true }) control!: FormControl;
-  @Input() label = '';
-  @Input() floatLabelVariant: FloatLabelVariants = FloatLabelVariants.On;
-  @Input() placeholder = '';
-  @Input() bigFontSize = false;
-  @Input() icon?: IconDefinition;
-  @Input() mask: InputMasks | null = null;
-  @Input() withClear = false;
+export class InputComponent implements ControlValueAccessor {
+  @Input() public label = '';
+  @Input() public floatLabelVariant: FloatLabelVariants = FloatLabelVariants.On;
+  @Input() public placeholder = '';
+  @Input() public bigFontSize = false;
+  @Input() public icon?: IconDefinition;
+  @Input() public mask: InputMasks | null = null;
+  @Input() public withClear = false;
   protected readonly faXmark = faXmark;
+  protected value: string | number = '';
+
+  public constructor(ngControl?: NgControl) {
+    if (ngControl) {
+      ngControl.valueAccessor = this;
+    }
+  }
 
   public get maskOptions(): MaskitoOptions | null {
     switch (this.mask) {
@@ -51,8 +63,7 @@ export class InputComponent {
         return null;
       case InputMasks.Number: {
         const numberOptions = maskitoNumberOptionsGenerator({
-          decimalSeparator: ',',
-          precision: 8,
+          precision: 0,
           min: 0,
         });
         return {
@@ -64,5 +75,30 @@ export class InputComponent {
         };
       }
     }
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  public onChange: (value: string | number) => void = () => {};
+
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  public onTouched: () => void = () => {};
+
+  public writeValue(value: string | number): void {
+    this.value = value || '';
+  }
+
+  public registerOnChange(fn: (value: string | number) => void): void {
+    this.onChange = fn;
+  }
+
+  public registerOnTouched(fn: () => void): void {
+    this.onTouched = fn;
+  }
+
+  public onInput(): void {
+    if (this.mask === 'number') {
+      this.value = parseInt(this.value.toString().replace(/\s/g, ''));
+    }
+    this.onChange(this.value);
   }
 }
